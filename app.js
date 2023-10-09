86b7a882cfbd59075f4887597c95c5e76d076e92
SandlerJr <sandlerjunin@gmail.com>
2023-10-08 21:36:23 -0300

    Criando novo projeto

---
+++ app.js
@@ -0,0 +1,8 @@
+alert ('Bem vindo ao jogo do número secreto');
+
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número de 1 a 30')
+
+if ( chute == numeroSecreto ) {
+    console.log('Parabéns você acertou o número secreto');
+}
