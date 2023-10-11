e5a41d2dabf29c9f25fd1ff9d2b5f08733ee984f
SandlerJr <sandlerjunin@gmail.com>
2023-10-11 15:45:07 -0300

    Atualizaçãoindependente: Realizado a validação de caractere inserido pelo usuario

--- app.js
+++ app.js
@@ -8,29 +8,27 @@ let tentativas = 1;
 
 while (chute != numeroSecreto){
     chute = prompt(`Escolha um número de 1 a ${numeroMax}`);
-    if ( chute == numeroSecreto ) {
-        break;
+    if (isNaN(chute)){
+    alert(`Caractere invalido, para passarmos dessa parte o caractere digitado precisa ser um número`)
+} else {
+    if((chute > numeroMax) || (chute < 0)) {
+        alert(`Esse número e invalido, número não se encontra dentro dos limites estabelecidos pelo jogo`)
+    } else {
+        if ( chute == numeroSecreto ) {
+            break;
         } else {
-        if (chute > numeroSecreto){
-            alert ('O número secreto é menor que o ' + chute);
-        } else{
-            if(chute < numeroSecreto){
-            alert ('O número secreto é maior que o ' + chute);
+            if (chute > numeroSecreto){
+                alert ('O número secreto é menor que o ' + chute);
+            } else{
+                if(chute < numeroSecreto){
+                alert ('O número secreto é maior que o ' + chute);
+                }
+                tentativas ++;
             }
-            tentativas ++;
         }
     }
-    
+}
 }
 
 let concordancia = tentativas = 1 ? 'tentativa' : 'tentativas'; 
 alert(`Parabens você acertou o numero ${numeroSecreto} com ${tentativas} ${concordancia} ` );
-
-/*
-if(tentativas =){
-    alert(`Parabéns você acertou o número secreto ${numeroSecreto} na primeira tentativa`)
-} else{
-      alert (`Parabéns você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
-} 
-*/
-  
