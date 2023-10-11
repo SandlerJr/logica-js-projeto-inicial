d4f6ff8d8c179878dcac866f1791abd0a37e32d4
SandlerJr <sandlerjunin@gmail.com>
2023-10-11 12:00:38 -0300

    Aprendemos a evitar código duplicado para exibir a palavra 'tentativa' no singular ou plural, utilizando Operador ternário / Descobrimos como utilizar a função Math.random() para gerar um número aleatório, permitindo a criação de desafios e jogos mais dinâmicos e variados.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 
 alert ('Bem vindo ao jogo do número secreto');
 
-let numeroSecreto = 29;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 let chute = ;
 let tentativas = 1;
 
 while (chute != numeroSecreto){
-    prompt('Escolha um número de 1 a 30');
+    prompt('Escolha um número de 1 a 100');
     if ( chute == numeroSecreto ) {
-        alert (`Parabéns você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
-    } else {
+        break;
+        } else {
         if (chute > numeroSecreto){
             alert ('O número secreto é menor que o ' + chute);
         } else{
@@ -21,3 +21,15 @@ while (chute != numeroSecreto){
     }
     
 }
+
+let concordancia = tentativas = 1 ? 'tentativa' : 'tentativas'; 
+alert(`Parabens você acertou o numero ${numeroSecreto} com ${tentativas} ${concordancia} ` );
+
+/*
+if(tentativas =){
+    alert(`Parabéns você acertou o número secreto ${numeroSecreto} na primeira tentativa`)
+} else{
+      alert (`Parabéns você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+} 
+*/
+  
