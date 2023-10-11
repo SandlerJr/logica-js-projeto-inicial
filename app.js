04c58050874325b4d23be627e5c1085d6eaa59cb
SandlerJr <sandlerjunin@gmail.com>
2023-10-11 10:07:55 -0300

    Realizado o termino da unidade 2(Condicionais e concatenação) e 3(Loops e tentativa) do curso Logigade programação: mergulhe em programação com JavaScript

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+
 alert ('Bem vindo ao jogo do número secreto');
 
 let numeroSecreto = 29;
-let chute = prompt('Escolha um número de 1 a 30')
+let chute = ;
+let tentativas = 1;
 
-if ( chute == numeroSecreto ) {
-    console.log('Parabéns você acertou o número secreto');
+while (chute != numeroSecreto){
+    prompt('Escolha um número de 1 a 30');
+    if ( chute == numeroSecreto ) {
+        alert (`Parabéns você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {
+        if (chute > numeroSecreto){
+            alert ('O número secreto é menor que o ' + chute);
+        } else{
+            if(chute < numeroSecreto){
+            alert ('O número secreto é maior que o ' + chute);
+            }
+            tentativas ++;
+        }
+    }
+    
 }
